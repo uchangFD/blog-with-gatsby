3dee562cbefcb8d34e313a3cc87f60eb29802784
uchang heo <ucheo@rsupport.com>
2019-04-18 10:47:54 +0900

    add gatsby(typescript, node-sass)

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div></div>
+)
+
+export default IndexPage
